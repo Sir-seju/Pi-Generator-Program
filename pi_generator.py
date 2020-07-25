e991d623a125e1e23a43c6e2d32a24c15661f414
Sir-seju <68641934+Sir-seju@users.noreply.github.com>
2020-07-26 00:20:27 +0100

    remove the limit placed on program input
    program now has no limit on amount of decimal places of pi

--- pi_generator.py
+++ pi_generator.py
@@ -5,7 +5,7 @@ import time
 
 def player_input():
     n = 0
-    while n==0 or n > 1000:
+    while n==0:
         try:
             n = int(input('To how many decimal places (less than 1000) do you want to find "Pi" in?: '))
         except:
@@ -13,16 +13,36 @@ def player_input():
             continue
     return n
 
-def pi_generator(n):
+def pi_generator(n,x=0):
     #generate pi to 'n' decimal places
     print(f'Generating pi to {n} decimal places...')
-    r = requests.get(f'https://api.pi.delivery/v1/pi?start=0&numberOfDigits={n}')
-    if not r.ok:
-        print('Sorry! Server down! Please try again later')
-    r_dict= r.json()
-    pi = r_dict['content']
-    Pi = pi[:1] + '.' + pi[1:]    
-    print(Pi)
+    if n>1000:
+        a = 1000
+        while n > 1000:
+            r = requests.get(f'https://api.pi.delivery/v1/pi?start={x}&numberOfDigits={a}')
+            if not r.ok:
+                print('Sorry! Server down! Please try again later')
+            r_dict= r.json()
+            pi = r_dict['content']
+            if x==0:
+                Pi = pi[:1] + '.' + pi[1:]
+                print(Pi, end = "")
+            else:       
+                print(pi, end = "")
+            n-=1000
+            x+=1000
+        r = requests.get(f'https://api.pi.delivery/v1/pi?start={x}&numberOfDigits={n}')
+        r_dict= r.json()
+        pi = r_dict['content']    
+        print(pi)
+    else:
+        r = requests.get(f'https://api.pi.delivery/v1/pi?start={x}&numberOfDigits={n}')
+        if not r.ok:
+            print('Sorry! Server down! Please try again later')
+        r_dict= r.json()
+        pi = r_dict['content']
+        Pi = pi[:1] + '.' + pi[1:]    
+        print(Pi)
 
 def play_again():
     while True:
